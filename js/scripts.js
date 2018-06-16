045efcd481ea1e483872d0e06431f9f5a3ebf427
Witold Olszyk <witoldolszyk@gmail.com>
2018-06-16 16:41:47 +0200

    website

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".btn1").click(function(){
+        $(".hide").slideUp(2500);
+
+});
+    $(".btn2").click(function(){
+        $(".show").fadeIn(3000).css("color", "#7D515A");
+            });
+});
